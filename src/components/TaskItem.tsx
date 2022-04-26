f2016e8110b0999caf35ef6fbdbcd65d7f69fd13
Shogo Naito <mimi_giza@yahoo.co.jp>
2022-04-27 04:27:24 +0900

    イベント処理の場所を変更

--- src/components/TaskItem.tsx
+++ src/components/TaskItem.tsx
@@ -2,14 +2,47 @@ import React from 'react'
 import {Task} from './Types'
 import DeleteIcon from '@mui/icons-material/Delete';
 import Card from '@mui/material/Card';
+import { useMutation, useQueryClient } from 'react-query';
+import axios from 'axios';
 
 type Props = {
     task: Task
-    handleDone: (event: any, task: Task) => void
-    handleDelete: (task: Task) => void
 }
 
-const TaskItem: React.FC<Props> = ({ task, handleDone, handleDelete }) => {
+const TaskItem: React.FC<Props> = ({ task }) => {
+
+    const queryClient = useQueryClient()
+    
+    const deleteMutation = useMutation((task: Task) => {
+        return axios.delete('http://localhost:8000/', { data: task })
+    }, {
+        onSuccess: response => {
+            queryClient.setQueryData(['tasks'], response.data)
+        }
+    })
+
+    const putMutation = useMutation((task: Task) => {
+        return axios.put('http://localhost:8000/' + task.id, task)
+    }, {
+        onSuccess: response => {
+            queryClient.setQueryData(['tasks'], response.data)
+        }
+    })
+
+    const handleDone = (event: any, task: Task) => {
+
+        let newTask: Task = {
+            id: task.id,
+            text: task.text,
+            state: event.target.checked ? "DONE" : "READY"
+        }
+        putMutation.mutate(newTask)
+    }
+
+    const handleDelete = (task: Task) => {
+        deleteMutation.mutate(task)
+    }
+
     return (
         <li>
             <Card sx={{ minWidth: 50 }}>
--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import { useMutation, useQuery, useQueryClient } from 'react-query'
+import { useQuery } from 'react-query'
 import TaskItem from './TaskItem'
 import { Task } from './Types'
 
@@ -13,44 +13,22 @@ function useTasks() {
 }
 
 const TaskList: React.FC = () => {
-    const queryClient = useQueryClient()
     const { isLoading, data, error, isFetching } = useTasks()
 
-    const deleteMutation = useMutation((task: Task) => {
-        return axios.delete('http://localhost:8000/', { data: task })
-    }, {
-        onSuccess: response => {
-            queryClient.setQueryData(['tasks'], response.data)
-        }
-    })
-
-    const putMutation = useMutation((task: Task) => {
-        return axios.put('http://localhost:8000/' + task.id, task)
-    }, {
-        onSuccess: response => {
-            queryClient.setQueryData(['tasks'], response.data)
-        }
-    })
-
-    const handleDone = (event: any, task: Task) => {
-        
-        let newTask:Task = {
-            id: task.id,
-            text: task.text,
-            state: event.target.checked ? "DONE" : "READY"
-        }
-        putMutation.mutate(newTask)
-    }
-
-    const handleDelete = (task: Task) => {
-       deleteMutation.mutate(task)
-    }
 
     if (isLoading) {
         return (
         <div>
             <span>Loading...</span>
         </div>
+        );
+    }
+
+    if (error) {
+        return (
+            <div>
+                <span>error</span>
+            </div>
         );
     }
 
@@ -62,8 +40,6 @@ const TaskList: React.FC = () => {
                         <TaskItem
                             key={task.id}
                             task={task}
-                            handleDone={handleDone}
-                            handleDelete={handleDelete}
                         />    
                     )
                 })
